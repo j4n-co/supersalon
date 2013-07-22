2209488af86d0e7f890607f7694a4f747e0af651
Jan Drewniak <jan.drewniak@gmail.com>
2013-07-22 09:48:28 -0400

    initial commit, clean spree 2 install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spree2::Application.config.secret_token = '1d110c64c94fdabcc0f558a7e1cf5fbb3cf185250aa74d5c3d130bc7c50d86b34fb54059dc2e9b71647da2e54e481f8471db460202d42a1d8b6bf19f1856938d'
