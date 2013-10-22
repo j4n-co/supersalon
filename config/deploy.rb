46fec9094b833c95a842b1ffc3725528498ca267
Jan Drewniak <jan.drewniak@gmail.com>
2013-10-22 12:10:37 -0400

    cappy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,14 +9,14 @@ set :use_sudo, false
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
+after "deploy", "deploy:cleanup", "deploy:rechown", "deploy:restart" # keep only the last 5 releases
 
 
 #local deploy to DigitalOcean
 set :scm, :git 
 set :repository, "git@github.com:j4n-co/supersalon.git" 
 #set :deploy_via, :copy 
-set :user, "rails"
+set :user, "root"
 
 set :server_ip, "37.139.10.22"
 server server_ip, :app, :web, :primary => true
@@ -29,12 +29,33 @@ set :rvm_type, :system
 
 set :bundle_without, [:development, :test, :acceptance]
 
+
+before "deploy:start" do
+  deploy.migrate
+end
+
 namespace :deploy do
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "service unicorn restart"
+  end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "service unicorn restart"
+  end
+
+  task :rechown, :roles => :app, :except => { :no_release => true } do
+    run "chown -R rails:www-data /home/rails"
+  end
+  
   namespace :assets do
+    
     task :precompile, :roles => :web, :except => { :no_release => true } do
       run "cd #{current_path} && #{rake} RAILS_ENV=#{rails_env} RAILS_GROUPS=assets assets:precompile --trace"
     end
+  
   end
+
 end
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
@@ -43,12 +64,6 @@ end
 #role :web, "your web-server here"                          # Your HTTP server, Apache/etc
 #role :app, "your app-server here"                          # This may be the same as your `Web` server
 #role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
 # namespace :deploy do
