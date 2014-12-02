18fd31b01029145bc35c29fa6cfa42f2a0c9b9e1
Jan Drewniak <jan.drewniak@gmail.com>
2014-12-02 13:42:59 -0500

    permalink validation on product update

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ ruby '2.1.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 
+gem 'rack-timeout'
+
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 gem 'mysql2'
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,3 +1,15 @@
 Spree::Product.class_eval do
     default_scope order("spree_products.available_on DESC")
- end
+
+    def check_valid_permalink 
+
+   		if self.permalink.present? 
+    		self.permalink = self.permalink.gsub(/[^0-9a-z,\-\ ]/i, '').squish.gsub(/ /i,'-').to_s.to_url
+   		else 
+   			self.permalink = name.to_s.to_url
+   		end 
+    end
+
+    before_update :check_valid_permalink
+
+end
