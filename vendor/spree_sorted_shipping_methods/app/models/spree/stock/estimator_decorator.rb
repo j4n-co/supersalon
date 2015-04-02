2212cd96ee021872d09ed1aa51922ced03785207
Jan Drewniak <jan.drewniak@gmail.com>
2015-04-02 19:44:13 +0200

    accommodating nil values in shipping method position

--- vendor/spree_sorted_shipping_methods/app/models/spree/stock/estimator_decorator.rb
+++ vendor/spree_sorted_shipping_methods/app/models/spree/stock/estimator_decorator.rb
@@ -2,12 +2,12 @@ Spree::Stock::Estimator.class_eval do
 	
 	def choose_default_shipping_rate(shipping_rates)
 	  unless shipping_rates.empty?
-		shipping_rates.sort_by{ |r| r.shipping_method.position }.first.selected = true
+		shipping_rates.sort_by{ |r| r.shipping_method.position.to_i }.first.selected = true
 	  end
 	end
 
 	def sort_shipping_rates(shipping_rates)
-	   shipping_rates.sort_by!{ |r| r.shipping_method.position }
+	   shipping_rates.sort_by!{ |r| r.shipping_method.position.to_i }
 	end
 
 end
