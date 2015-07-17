3afc548711967352c8a9674e8ac3bcd11659d615
Jan Drewniak <jan.drewniak@gmail.com>
2015-07-17 16:07:56 +0200

    fixing migration issues

--- db/migrate/20150518131405_create_spree_pages.rb
+++ db/migrate/20150518131405_create_spree_pages.rb
@@ -1,20 +1,23 @@
 class CreateSpreePages < ActiveRecord::Migration
   def self.up
-    change_table :spree_pages do |t|
-      t.string     :title
-      t.string     :nav_title
-      t.string     :path
-      
-      t.string     :meta_title
-      t.string     :meta_description
-      t.string     :meta_keywords
-      
-      t.integer    :position,   :default => 999
-      t.boolean    :accessible, :default => true
-      t.boolean    :visible,    :default => true
-      
-      t.timestamps
-    end
+    unless column_exists? :spree_pages, :title
+
+      change_table :spree_pages do |t|
+        t.string     :title
+        t.string     :nav_title
+        t.string     :path
+        
+        t.string     :meta_title
+        t.string     :meta_description
+        t.string     :meta_keywords
+        
+        t.integer    :position,   :default => 999
+        t.boolean    :accessible, :default => true
+        t.boolean    :visible,    :default => true
+        
+        t.timestamps
+      end
+    end 
     
   end
 
