d378c022b7bf61282af225333ebfc4ef650cc98b
Jan Drewniak <jan.drewniak@gmail.com>
2015-07-17 16:05:12 +0200

    fixing migration errors

--- db/migrate/20150330183350_add_sorted_order_to_spree_shipping_methods.spree_sorted_shipping_methods.rb
+++ db/migrate/20150330183350_add_sorted_order_to_spree_shipping_methods.spree_sorted_shipping_methods.rb
@@ -1,6 +1,8 @@
 # This migration comes from spree_sorted_shipping_methods (originally 20150330182517)
 class AddSortedOrderToSpreeShippingMethods < ActiveRecord::Migration
   def change
-    add_column :spree_shipping_methods, :position, :integer
+    unless column_exists? :spree_shipping_methods, :position
+    	add_column :spree_shipping_methods, :position, :integer
+    end
   end
 end
